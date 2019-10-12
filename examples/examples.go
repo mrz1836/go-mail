37916d8289f277924c56b02637f8a285dcd7d7f3
mrz1836 <mrz1818@gmail.com>
2019-10-12 15:54:33 -0400

    Test all three providers

--- examples/examples.go
+++ examples/examples.go
@@ -16,10 +16,19 @@ func main() {
 
 	// Define your service configuration
 	mail := new(gomail.MailService)
-	mail.MandrillAPIKey = os.Getenv("EMAIL_MANDRILL_KEY") //abc123
 	mail.FromName = "No Reply"
 	mail.FromUsername = "no-reply"
 	mail.FromDomain = os.Getenv("EMAIL_FROM_DOMAIN") //example.com
+
+	// Mandrill
+	mail.MandrillAPIKey = os.Getenv("EMAIL_MANDRILL_KEY") //aOfw3WU...
+
+	// AWS SES
+	mail.AwsSesAccessID = os.Getenv("EMAIL_AWS_SES_ACCESS_ID")   //AKIAY...
+	mail.AwsSesSecretKey = os.Getenv("EMAIL_AWS_SES_SECRET_KEY") //tOpw3WU...
+
+	// Postmark
+	mail.PostmarkServerToken = os.Getenv("EMAIL_POSTMARK_SERVER_TOKEN") //AKIAY...
 
 	// Startup the services
 	mail.StartUp()
@@ -37,6 +46,7 @@ func main() {
 	email.RecipientsBcc = []string{os.Getenv("EMAIL_TEST_BCC_RECIPIENT")}
 	email.Subject = "alert broadcast system - test message"
 	email.Tags = []string{"admin_alert"}
+	email.Important = true
 
 	// Add an attachment
 	f, err := os.Open("test-attachment-file.txt")
@@ -46,10 +56,11 @@ func main() {
 		email.AddAttachment("test-attachment-file.txt", "text/plain", f)
 	}
 
-	// Send the email
-	err = mail.SendEmail(email, gomail.Mandrill)
+	// Send the email (basic example using one provider)
+	provider := gomail.Postmark // AwsSes Mandrill
+	err = mail.SendEmail(email, provider)
 	if err != nil {
-		logger.Data(2, logger.ERROR, fmt.Sprintf("error in SendEmail: %s", err.Error()))
+		logger.Data(2, logger.ERROR, fmt.Sprintf("error in SendEmail: %s using provider %x", err.Error(), provider))
 	}
 
 	// Congrats!
