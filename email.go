7814e5b964dde5a00386d3e12304b40c3d108f49
mrz1836 <mrz1818@gmail.com>
2019-10-12 15:53:14 -0400

    Added the safegaurd and more providers

--- email.go
+++ email.go
@@ -52,6 +52,7 @@ func (m *MailService) NewEmail() (email *Email) {
 	email.Important = m.Important
 	email.TrackClicks = m.TrackClicks
 	email.TrackOpens = m.TrackOpens
+	email.ReplyToAddress = email.FromAddress
 
 	return
 }
@@ -62,9 +63,34 @@ func (m *MailService) SendEmail(email *Email, provider ServiceProvider) (err err
 	// Do we have that provider?
 	if containsServiceProvider(m.AvailableProviders, provider) {
 
-		// Send using Mandrill
+		// Safe guard the user sending mis-configured emails
+		if len(email.Subject) == 0 {
+			err = fmt.Errorf("email is missing a subject")
+			return
+		} else if len(email.PlainTextContent) == 0 && len(email.HTMLContent) == 0 {
+			err = fmt.Errorf("email is missing content (plain & html)")
+			return
+		} else if len(email.Recipients) == 0 {
+			err = fmt.Errorf("email is a recipient")
+			return
+		} else if len(email.Recipients) > maxToRecipients {
+			err = fmt.Errorf("max TO recipient limit of %d reached: %d", maxToRecipients, len(email.Recipients))
+			return
+		} else if len(email.RecipientsCc) > maxCcRecipients {
+			err = fmt.Errorf("max CC recipient limit of %d reached: %d", maxCcRecipients, len(email.RecipientsCc))
+			return
+		} else if len(email.RecipientsBcc) > maxBccRecipients {
+			err = fmt.Errorf("max BCC recipient limit of %d reached: %d", maxBccRecipients, len(email.RecipientsBcc))
+			return
+		}
+
+		// Send using given provider
 		if provider == Mandrill {
 			err = m.sendWithMandrill(email)
+		} else if provider == AwsSes {
+			err = m.sendWithAwsSes(email)
+		} else if provider == Postmark {
+			err = m.sendWithPostmark(email)
 		}
 	} else {
 		err = fmt.Errorf("service provider: %x was not in the list of available service providers: %x, email not sent", provider, m.AvailableProviders)
